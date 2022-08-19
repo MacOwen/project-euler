0f89b1e899a79534e71dcb4e0213d13c6e5538e8
Mac <mac@pro.local>
2022-08-19 13:18:11 -0700

    Starting over

--- 6_sum_square_difference.py
+++
@@ -1,8 +0,0 @@
-sum_of_squares = 0
-for i in range(1,101):
-    sum_of_squares += i*i
-
-square_of_sums = (100*(101))//2
-square_of_sums *= square_of_sums
-
-print(f"Result: {square_of_sums-sum_of_squares}")
